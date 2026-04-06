27ed47e57fea90d0f93ea2058fd7c895cc1bd8ed
David Antonio González <dgonzaleza007@gmail.com>
2026-04-05 21:12:40 -0600

    feat(validation): add logic to message errors

---
+++ resources/lang/es/validation.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'accepted' => 'El campo :attribute debe ser aceptado.',
+    'required' => 'El campo :attribute es obligatorio.',
+    'string' => 'El campo :attribute debe ser una cadena de texto.',
+    'max' => [
+        'string' => 'El campo :attribute no puede tener más de :max caracteres.',
+    ],
+    'unique' => 'El campo :attribute ya ha sido tomado.',
+
+    'attributes' => [
+        'name' => 'Nombre',
+        'description' => 'Descripción',
+    ],
+];
