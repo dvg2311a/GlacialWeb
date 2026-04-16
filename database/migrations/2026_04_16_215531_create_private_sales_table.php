ef84ca5d73b071d0b689770b6e88d29514a61f3a
dvg2311a <dgonzaleza007@gmail.com>
2026-04-16 16:20:33 -0600

    feat(/migration/privatre_sales): add new migration

---
+++ database/migrations/2026_04_16_215531_create_private_sales_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('private_sales', function (Blueprint $table) {
+            $table->bigIncrements("id");
+            $table->dateTime("date_sale")->index();
+
+            $table->foreignId("user_i")->constrained("users")->onUpdate("cascade")->onDelete("cascade");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('private_sales');
+    }
+};
---
+++ database/migrations/2026_04_16_220157_create_private_sale_details_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('private_sale_details', function (Blueprint $table) {
+            $table->bigIncrements("id");
+            $table->integer("quantity");
+            $table->decimal("unit_price");
+            $table->enum("type_price", ["Detalle", "Mayorista", "Malo"]);
+
+            $table->foreignId("product_id")->constrained("products")->onDelete("cascade")->onUpdate("cascade");
+            $table->foreignId("private_sale_id")->constrained("private_sales")->onDelete("cascade")->onUpdate("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('private_sale_details');
+    }
+};
