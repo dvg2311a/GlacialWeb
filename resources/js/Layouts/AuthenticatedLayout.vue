b6993f93160cface6bdc5fe173f0d1820005ec2d
David Antonio González <dgonzaleza007@gmail.com>
2026-05-04 21:42:16 -0600

    delete(authenticatedlayout): delete navbar

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,11 +1,8 @@
 <script setup>
-import { Fan, Snowflake, PencilRuler, Star, BetweenVerticalStart, ChartColumnStacked } from 'lucide-vue-next';
 import { ref } from 'vue';
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-import Dropdown from '@/Components/Dropdown.vue';
-import DropdownLink from '@/Components/DropdownLink.vue';
-import NavLink from '@/Components/NavLink.vue';
+
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+import Sidebar from '@/Components/Sidebar.vue';
 
 defineProps({
     title: String,
@@ -16,124 +13,9 @@ const showingNavigationDropdown = ref(false);
 </script>
 
 <template>
-    <div>
-        <div class="min-h-screen bg-gray-100">
-            <nav class="border-b border-gray-100 bg-white">
-                <!-- Primary Navigation Menu -->
-                <div class="mx-auto max-w-7x2 px-4 sm:px-6 lg:px-8">
-                    <div class="flex h-16 justify-between">
-                        <div class="flex">
-
-                            <!-- Navigation Links -->
-                            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                                    Dashboard
-                                </NavLink>
-                            </div>
-
-                            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex"
-                                v-if="$page.props.auth.roles.includes('Administrador')">
-                                <NavLink :href="route('roles.index')" :active="route().current('roles.index')">
-                                    <Star class="mr-2" /> Roles
-                                </NavLink>
-                            </div>
-
-                            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex"
-                                v-if="$page.props.auth.roles.includes('Administrador')">
-                                <NavLink :href="route('type_freezers.index')"
-                                    :active="route().current('type_freezers.index')">
-                                    <Fan class="mr-2" /> Tipos de Freezer
-                                </NavLink>
-                            </div>
-
-                            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex"
-                                v-if="$page.props.auth.roles.includes('Administrador')">
-                                <NavLink :href="route('freezers.index')" :active="route().current('freezers.index')">
-                                    <Snowflake class="mr-2" /> Freezers
-                                </NavLink>
-                            </div>
-
-                            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex"
-                                v-if="$page.props.auth.roles.includes('Administrador')">
-                                <NavLink :href="route('plate_dimensions.index')"
-                                    :active="route().current('plate_dimensions.index')">
-                                    <PencilRuler class="mr-2" /> Dimensiones de Placa
-                                </NavLink>
-                            </div>
-
-                            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex"
-                                v-if="$page.props.auth.roles.includes('Administrador')">
-                                <NavLink :href="route('plates.index')" :active="route().current('plates.index')">
-                                    <BetweenVerticalStart class="mr-2" /> Placas
-                                </NavLink>
-                            </div>
-
-                            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex"
-                                v-if="$page.props.auth.roles.includes('Administrador')">
-                                <NavLink :href="route('type_products.index')" :active="route().current('type_products.index')">
-                                    <ChartColumnStacked class="mr-2" /> Tipos de Producto
-                                </NavLink>
-                            </div>
-
-                        </div>
-
-                        <div class="hidden sm:ms-6 sm:flex sm:items-center">
-                            <!-- Settings Dropdown -->
-                            <div class="relative ms-3">
-                                <Dropdown align="right" width="48">
-                                    <template #trigger>
-                                        <span class="inline-flex rounded-md">
-                                            <button type="button"
-                                                class="inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none">
-                                                {{ $page.props.auth.user.name }}
-
-                                                <svg class="-me-0.5 ms-2 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                                    viewBox="0 0 20 20" fill="currentColor">
-                                                    <path fill-rule="evenodd"
-                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                        clip-rule="evenodd" />
-                                                </svg>
-                                            </button>
-                                        </span>
-                                    </template>
-
-                                    <template #content>
-                                        <DropdownLink :href="route('profile.edit')">
-                                            Profile
-                                        </DropdownLink>
-                                        <DropdownLink :href="route('logout')" method="post" as="button">
-                                            Log Out
-                                        </DropdownLink>
-                                    </template>
-                                </Dropdown>
-                            </div>
-                        </div>
-
-                        <!-- Hamburger -->
-                        <div class="-me-2 flex items-center sm:hidden">
-                            <button @click="
-                                showingNavigationDropdown =
-                                !showingNavigationDropdown
-                                "
-                                class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 transition duration-150 ease-in-out hover:bg-gray-100 hover:text-gray-500 focus:bg-gray-100 focus:text-gray-500 focus:outline-none">
-                                <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                                    <path :class="{
-                                        hidden: showingNavigationDropdown,
-                                        'inline-flex':
-                                            !showingNavigationDropdown,
-                                    }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M4 6h16M4 12h16M4 18h16" />
-                                    <path :class="{
-                                        hidden: !showingNavigationDropdown,
-                                        'inline-flex':
-                                            showingNavigationDropdown,
-                                    }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M6 18L18 6M6 6l12 12" />
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
-                </div>
+    <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+            <nav class="border-b border-gray-100 dark:border-gray-700 bg-white dark:bg-gray-800">
 
                 <!-- Responsive Navigation Menu -->
                 <div :class="{
@@ -144,42 +26,48 @@ const showingNavigationDropdown = ref(false);
                         <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
                             Dashboard
                         </ResponsiveNavLink>
-                    </div>
-
-                    <!-- Responsive Settings Options -->
-                    <div class="border-t border-gray-200 pb-1 pt-4">
-                        <div class="px-4">
-                            <div class="text-base font-medium text-gray-800">
-                                {{ $page.props.auth.user.name }}
-                            </div>
-                            <div class="text-sm font-medium text-gray-500">
-                                {{ $page.props.auth.user.email }}
-                            </div>
-                        </div>
-
-                        <div class="mt-3 space-y-1">
-                            <ResponsiveNavLink :href="route('profile.edit')">
-                                Profile
+                        <template v-if="$page.props.auth.roles.includes('Administrador')">
+                            <ResponsiveNavLink :href="route('roles.index')" :active="route().current('roles.index')">
+                                Roles
                             </ResponsiveNavLink>
-                            <ResponsiveNavLink :href="route('logout')" method="post" as="button">
-                                Log Out
+                            <ResponsiveNavLink :href="route('type_freezers.index')" :active="route().current('type_freezers.index')">
+                                Tipos de Freezer
                             </ResponsiveNavLink>
-                        </div>
+                            <ResponsiveNavLink :href="route('freezers.index')" :active="route().current('freezers.index')">
+                                Freezers
+                            </ResponsiveNavLink>
+                            <ResponsiveNavLink :href="route('plate_dimensions.index')" :active="route().current('plate_dimensions.index')">
+                                Dimensiones de Placa
+                            </ResponsiveNavLink>
+                            <ResponsiveNavLink :href="route('plates.index')" :active="route().current('plates.index')">
+                                Placas
+                            </ResponsiveNavLink>
+                            <ResponsiveNavLink :href="route('type_products.index')" :active="route().current('type_products.index')">
+                                Tipos de Producto
+                            </ResponsiveNavLink>
+                            <ResponsiveNavLink :href="route('flavor_products.index')" :active="route().current('flavor_products.index')">
+                                Sabor de Producto
+                            </ResponsiveNavLink>
+                            <ResponsiveNavLink :href="route('status_products.index')" :active="route().current('status_products.index')">
+                                Estado de Producto
+                            </ResponsiveNavLink>
+                        </template>
                     </div>
                 </div>
             </nav>
 
-            <!-- Page Heading -->
-            <header class="bg-white shadow" v-if="$slots.header">
-                <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                    <slot name="header" />
-                </div>
-            </header>
-
             <!-- Page Content -->
-            <main>
-                <slot />
-            </main>
+            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                <div class="flex gap-6 ">
+
+                    <Sidebar />
+
+                    <!-- Main content -->
+                    <main class="flex-1 ml-80">
+                        <slot />
+                    </main>
+                </div>
+            </div>
         </div>
     </div>
 </template>
