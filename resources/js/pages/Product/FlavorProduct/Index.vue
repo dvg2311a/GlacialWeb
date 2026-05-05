141458109322e76d234108cb8caa021fb080e7a1
David Antonio González <dgonzaleza007@gmail.com>
2026-05-04 22:46:14 -0600

    fix(flavorproduct): correction color in dark mode

--- resources/js/pages/Product/FlavorProduct/Index.vue
+++ resources/js/pages/Product/FlavorProduct/Index.vue
@@ -38,10 +38,10 @@ function confirmDelete(id) {
     <AuthenticatedLayout>
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 bg-white border-b border-gray-200">
+                <div class=" overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-1 ml-4">
                         <h1 class="text-2xl font-bold mb-4">Sabores de Productos</h1>
-                        <p class="text-gray-600">En este apartado puedes gestionar los sabores de productos.
+                        <p class="dark:text-white">En este apartado puedes gestionar los sabores de productos.
                             Agregar, editar ver y eliminar cada registro.</p>
                     </div>
                 </div>
