0bdd68f64e9b80593f406e7ae9ae0dd6de9fbfa0
dvg2311a <dgonzaleza007@gmail.com>
2026-05-14 15:40:05 -0600

    feat(orderenterprisecontroller): add edit and update

--- app/Http/Controllers/OrderEnterpriseController.php
+++ app/Http/Controllers/OrderEnterpriseController.php
@@ -16,7 +16,7 @@ class OrderEnterpriseController extends Controller
 
     public function index()
     {
-        $order = OrderEnterprise::query()->with('purchaseOrderDetail:id,boxes_received,purchase_price_total_per_box,order_enterprise_id,product_id')->get();
+        $order = OrderEnterprise::query()->with('purchaseOrderDetail:id,boxes_received,purchase_price_total_per_box,order_enterprise_id,product_id')->latest()->get();
 
         return Inertia::render('OrderEnterprise/Index', ['order' => $order]);
     }
@@ -105,12 +105,88 @@ class OrderEnterpriseController extends Controller
 
     public function edit(string $id)
     {
-        //
+        try {
+            $order = OrderEnterprise::with('purchaseOrderDetail')->findOrFail($id);
+
+            $purchase = PurchaseOrderDetail::where('order_enterprise_id', $order->id)->with('product')->get();
+            $products = Product::all();
+
+            return Inertia::render('OrderEnterprise/Edit', [
+                'order' => $order,
+                'purchase' => $purchase,
+                'products' => $products,
+            ]);
+        } catch (Throwable $e) {
+            return back()->with('error', 'Ocurrió un error al ingresar a la vista el pedido');
+        }
     }
 
-    public function update(Request $request, string $id)
+    public function update(OrderEnterpriseRequest $request, string $id)
     {
-        //
+        try {
+
+            DB::transaction(function () use ($request, $id) {
+
+                $order = OrderEnterprise::findOrFail($id);
+
+                $order->update([
+                    'order_date' => $request->order_date,
+                    'expected_date' => $request->expected_date,
+                    'order_status' => $request->order_status,
+                ]);
+
+
+                $order->purchaseOrderDetail()->delete();
+
+                $totalTaxGeneral = 0;
+                $subtotalBaseGeneral = 0;
+                $grandTotalGeneral = 0;
+
+                foreach ($request->products as $product) {
+
+                    $taxAmount = $product['purchase_price_per_box'] * ($product['tax_percentage'] / 100);
+
+                    $totalPerBox = $product['purchase_price_per_box'] + $taxAmount;
+
+                    $totalTax = $taxAmount * $product['boxes_received'];
+                    $totalTaxGeneral += $totalTax;
+
+                    $subtotalBase = $product['boxes_received'] * $product['purchase_price_per_box'];
+                    $subtotalBaseGeneral += $subtotalBase;
+
+                    $grandTotal = $product['boxes_received'] * $totalPerBox;
+                    $grandTotalGeneral += $grandTotal;
+
+
+                    // recrear detalle
+                    PurchaseOrderDetail::create([
+                        'boxes_ordered' => $product['boxes_ordered'],
+                        'boxes_received' => $product['boxes_received'],
+                        'purchase_price_per_box' => $product['purchase_price_per_box'],
+                        'tax_percentage' => $product['tax_percentage'],
+                        'tax_amount_per_box' => $taxAmount,
+                        'purchase_price_total_per_box' => $totalPerBox,
+                        'order_enterprise_id' => $order->id,
+                        'product_id' => $product['id'],
+                    ]);
+                }
+
+                // actualizar totales
+                $order->update([
+                    'total_tax' => $totalTaxGeneral,
+                    'subtotal_base' => $subtotalBaseGeneral,
+                    'grand_total' => $grandTotalGeneral,
+                ]);
+            });
+
+            return redirect()
+                ->route('orders_enterprises.index')
+                ->with('success', 'Pedido actualizado exitosamente');
+        } catch (Throwable $e) {
+            report($e);
+
+            return back()->withInput()->with('error', 'Ocurrió un error al actualizar el pedido');
+        }
     }
 
     public function destroy(string $id)
