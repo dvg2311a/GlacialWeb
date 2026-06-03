a68dcd21e779648844e46e051862064b3a70485a
David Antonio González <dgonzaleza007@gmail.com>
2026-06-03 00:29:01 -0600

    feat(public): add build

---
+++ public/build/assets/InputError-Bk3QOWbr.js
@@ -0,0 +1,1 @@
+import{D as e,P as t,R as n,h as r,u as i,v as a}from"./app-BQEAxISy.js";var o={class:`text-sm text-red-600`},s={__name:`InputError`,props:{message:{type:String}},setup(s){return(c,l)=>t((e(),a(`div`,null,[r(`p`,o,n(s.message),1)],512)),[[i,s.message]])}};export{s as t};
