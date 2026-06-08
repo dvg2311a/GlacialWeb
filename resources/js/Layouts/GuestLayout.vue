001ae0eb25b0375f4899ad793817a3db44115281
Your Name <dgonzaleza007@gmail.com>
2026-06-08 09:30:05 -0600

    delete(guestlayout): delete link

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { Link } from '@inertiajs/vue3';
 import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 </script>
 
@@ -8,9 +7,9 @@ import ApplicationLogo from '@/Components/ApplicationLogo.vue';
         class="flex min-h-screen flex-col items-center dark:bg-gray-900 pt-6 sm:justify-center sm:pt-0"
     >
         <div>
-            <Link href="/">
+
                 <ApplicationLogo class="h-20 w-20 fill-current text-gray-500" />
-            </Link>
+
         </div>
 
         <div
