5c1b122e11a0297458e5dbfc5c413f74d6870cd6
dvg2311a <dgonzaleza007@gmail.com>
2026-06-30 07:41:01 -0600

    fix(cartrequest): correction in the validation rules

--- app/Http/Requests/CartRequest.php
+++ app/Http/Requests/CartRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class CartRequest extends FormRequest
 {
@@ -16,7 +17,8 @@ class CartRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'plate_number' => 'required|string|max:20|unique:carts,plate_number,' . $this->route('cart'),
+
+            'plate_number' => ['required','string', 'max:20', Rule::unique('carts', 'plate_number')->ignore($this->route('cart'))],
             'description' => 'nullable|string|max:50',
             'proprietary_type' => 'required|in:Company,Individual',
             'type_cart_id' => 'required|exists:type_carts,id',
